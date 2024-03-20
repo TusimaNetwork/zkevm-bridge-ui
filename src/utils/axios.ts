3623044d126ba8a4816203a981876a1b3b4c5b0e
light10917 <light10917@gmail.com>
2024-03-20 18:34:52 +0800

    add axios api

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,17 +2,17 @@ import axios from 'axios'
 
 
 // export const baseURL = isLocalhost ? getEnv('REACT_APP_DEV_REQUEST_URL') : URLs.join('')
-export const timeout = 10000
+export const timeout = 1000
 export const contentType = 'application/x-www-form-urlencoded'
-// axios.defaults.baseURL = baseURL
+axios.defaults.baseURL = '/api'
 axios.defaults.timeout = timeout
 axios.defaults.headers.post['Content-Type'] =  'application/json'
-// axios.defaults.headers.post['Accept'] = 'application/json'
-// axios.defaults.headers.get['Cache-Control'] = 'no-cache'
-// axios.defaults.headers.get['Pragma'] = 'no-cache'
+// axios.defaults.headers.post ={} 
+// axios.defaults.headers.get = {}
 // axios.defaults.headers.get['Cache-Control'] = 'no-cache'
 axios.interceptors.request.use(
   function(config) {
+    config.url = '/api' + config.url; // 添加前缀
     return config
   },
   function(error) {
